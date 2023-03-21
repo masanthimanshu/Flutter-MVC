c23e00c8bfe4c15c908843823683276f2ffc3519
Himanshu Masant <himanshumasant@uxdlabtech.com>
2023-03-21 23:40:58 +0530

    updates

---
+++ lib/Network/endpoints.dart
@@ -0,0 +1,3 @@
+class ApiEndpoints {
+  String homeEndpoint = "https://jsonplaceholder.typicode.com/posts";
+}
--- lib/Screens/home/Controller/controller.dart
+++ lib/Screens/home/Controller/controller.dart
@@ -1,14 +1,12 @@
-import 'package:flutter_mvc/service/network.dart';
+import 'package:flutter_mvc/Network/requests.dart';
+import 'package:flutter_mvc/Network/endpoints.dart';
 import 'package:flutter_mvc/Screens/home/Model/model.dart';
 
 class DataController {
   Future<List<DataModel>?> getData() async {
-    final data = await GetRequest().getJsonData(
-      "https://jsonplaceholder.typicode.com/posts",
-    );
+    final data = await GetRequest().getJsonData(ApiEndpoints().homeEndpoint);
 
     if (data != null) return dataModelFromJson(data);
-
     return null;
   }
 }
